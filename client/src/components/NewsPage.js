cc2f6c3a90c38dfabbdc132e96b6219024712629
Satyam Yadav <satyamok03@gmail.com>
2025-02-19 13:32:59 +0530

    #updated the api to frontend

--- client/src/components/NewsPage.js
+++ client/src/components/NewsPage.js
@@ -14,7 +14,7 @@ const NewsPage = () => {
   const fetchNews = async (category) => {
     setLoading(true);
     try {
-      const response = await fetch(`http://localhost:5000/api/news?category=${category}`);
+      const response = await fetch(`https://newshub-jjj5.onrender.com/api/news?category=${category}`);
       const data = await response.json();
       setNews(data.articles || []);
       setLoading(false);
